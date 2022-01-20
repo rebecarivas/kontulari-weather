5262ba8af9b6e7c697fe10c7d4af0dfbbdc3ffcc
rebecarivas <rebecarivas@gmail.com>
2022-01-20 18:33:50 -0300

    chore: adiciona componentes no App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
   <> 
     <Header />
+    <SearchBar />
+    <Footer />
    
   </>
   );
