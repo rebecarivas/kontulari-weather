6399e77d84f8c2ced9f080cf51eefabde14c4742
rebecarivas <rebecarivas@gmail.com>
2022-01-31 08:12:28 -0300

    feature: adiciona o test

---
+++ client/src/__test__/index.spec.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import dark from '../style/themes/dark';
+import App from '../App'
+
+const AppRender = () => render(
+    <ThemeProvider theme={dark}>
+        <App/>
+    </ThemeProvider>);
+
+describe('rendering elements', () => {
+    test('testing react search autocomplete', () => {
+        AppRender()
+        const cityName = screen.getByPlaceholderText('Enter a city name') 
+        fireEvent.change(cityName, { target: { value: 'Salvador' } });
+        expect(cityName.value).toEqual('Salvador');
+    })
+})
